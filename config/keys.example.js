52d45458f5771d5b5cb0f4dc656582d36c31f900
kbastamow <bastamowkat@gmail.com>
2023-04-20 15:42:12 +0200

    Server & database connection established. Base of main.

---
+++ config/keys.example.js
@@ -0,0 +1,6 @@
+
+/*
+module.exports = {
+    MONGO_URI: "mongodb+srv://<username>:<password>.hxyopnd.mongodb.net/yourDBName" 
+}
+*/
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-//Hello
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const { dbConnection } = require("./config/config")
+
+app.use(express.json())
+
+dbConnection()
+
+
+
+app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
