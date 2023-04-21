d09ecce275a328bb88e77a6cddfd030502695591
kbastamow <bastamowkat@gmail.com>
2023-04-21 12:26:49 +0200

    Authentication middleware

--- config/keys.example.js
+++ config/keys.example.js
@@ -1,6 +1,7 @@
 
 /*
 module.exports = {
-    MONGO_URI: "mongodb+srv://<username>:<password>.hxyopnd.mongodb.net/yourDBName" 
+    MONGO_URI: "mongodb+srv://<username>:<password>.hxyopnd.mongodb.net/yourDBName",
+    jwt_secret:'Your password'   
 }
 */
---
+++ middlewares/authentication.js
@@ -0,0 +1,27 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const jwt_secret = require("../config/keys.js");
+
+
+
+
+const authentication = async(res, req, next) => {
+    try{
+        const token = req.headers.authorization;
+        const payload = jwt.verify(token, jwt_secret);
+        const user = await User.findOne({_id: payload._id, tokens: token })
+        
+        if(!user) {
+            return res.status(401).send("Unauthorised request");
+        }
+
+        req.user = user;
+        next();
+    } catch(error){
+        console.error(error);
+        res.status(500).send("Error with token");
+    }
+}
+
+
+module.exports = { authentication }
