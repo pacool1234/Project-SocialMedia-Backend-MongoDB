d3de95217636ec898c6b6d7e6bcaad099e666dd4
kbastamow <bastamowkat@gmail.com>
2023-04-23 10:41:21 +0200

    multer programmed + added to createpost

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -2,11 +2,13 @@ const Post = require('../models/Post');
 
 
 
+
+
 const PostController = {
 
     async create(req, res) {  //add 'required' to model
         try {
-            const post = await Post.create({ ...req.body, userId: req.user._id });
+            const post = await Post.create({ ...req.body, userId: req.user._id, image: req.file.path });
             res.status(201).send({ msg: 'New post created', post });
         } catch (error) {
             console.error(error);
--- routes/posts.js
+++ routes/posts.js
@@ -2,9 +2,10 @@ const express = require('express');
 const PostController = require('../controllers/PostController');
 const router = express.Router();
 const { authentication, isAuthor } = require('../middlewares/authentication');
+const {uploadPostImg} = require('../middlewares/upload');
 
 
-router.post('/create', authentication, PostController.create);
+router.post('/create', authentication, uploadPostImg.single('image'), PostController.create);
 router.put('/update/:_id', authentication, isAuthor, PostController.update);
 router.delete('/delete/:_id', authentication, isAuthor, PostController.delete);
 router.get('/getAll', PostController.getAll)
