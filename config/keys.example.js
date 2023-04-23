104394fe3b1c36b9853db1ea69628c5c6e68e47a
kbastamow <bastamowkat@gmail.com>
2023-04-23 11:13:53 +0200

    PostCreate: upload with or without image without error thrown

--- config/keys.example.js
+++ config/keys.example.js
@@ -1,6 +1,6 @@
 /*
 module.exports = {
-    MONGO_URI: "mongodb+srv://<username>:<password>.hxyopnd.mongodb.net/yourDBName",
+    MONGO_URI: 'mongodb+srv://<username>:<password>.hxyopnd.mongodb.net/yourDBName',
     jwt_secret:'Your password'.
      auth: {
         'user': 'your email',
