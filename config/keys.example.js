b31e0664210d30d4d0121d6a9cda10cf488215c6
kbastamow <bastamowkat@gmail.com>
2023-04-27 13:55:36 +0200

    dotenv in use

--- config/keys.example.js
+++ config/keys.example.js
@@ -1,4 +1,5 @@
-/*
+/* THIS FILE IS REDUNDANT NOW THAT .ENV is in use
+
 module.exports = {
     MONGO_URI: 'mongodb+srv://<username>:<password>.hxyopnd.mongodb.net/yourDBName',
     jwt_secret:'Your password'.
