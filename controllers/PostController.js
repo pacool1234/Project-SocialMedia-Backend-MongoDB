3bc4c2196da7186472d3be51252d72738af9d0ff
kbastamow <bastamowkat@gmail.com>
2023-04-27 13:24:28 +0200

    little fixes with post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -8,11 +8,12 @@ const PostController = {
 
     async create(req, res) {  //add 'required' to model
         try {
+            console.log("HELLO")
             let data = req.body;  
             if(req.file){            //check if file exists, if not, simply take info from body!
                 data = {...req.body, image: req.file.filename }
             }
-            const post = await Post.create({ ...data, userId: req.user._id});
+            const post = await Post.create({ ...data, userId: req.user._id});  
             res.status(201).send({ msg: 'New post created', post });
         } catch (error) {
             console.error(error);
